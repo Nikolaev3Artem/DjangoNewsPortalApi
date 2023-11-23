b225dad55a582e8246697ab5a21df9d80c008b0b
Nikolaev3Artem <artnik349@gmail.com>
2023-11-23 21:39:44 +0200

    resolved some conflicts with parse_news command

--- news/management/commands/parse_news.py
+++ news/management/commands/parse_news.py
@@ -14,37 +14,42 @@ class Command(BaseCommand):
             for news in data['results']:
                 if news != "message":
                     try:
-                        # print(f"Image Url: {news['image_url']} \n\
-                        #     Content: {news['content']} \n\
-                        #     Creator: {news['creator']} \n\
-                        #     Pub Date: {news['pubDate']} \n\
-                        #     Description: {news['description']} \n\
-                        #     Country: {news['country']}\n\n\n")
+
                         
                         if news['image_url'] and \
                             len(news['content']) != 0 and \
                             news['pubDate'] and \
                             news['description'] and \
                             news['country'] and \
-                            news['image_url'][0:4] == 'https':
+                            news['image_url'][0:5] == 'https':
+                            print(f" \
+                                Link: {news['link']} \n\
+                                Title: {news['title']} \n\
+                                Image Url: {news['image_url']} \n\
+                                Content: {news['content']} \n\
+                                Creator: {news['creator']} \n\
+                                Pub Date: {news['pubDate']} \n\
+                                Description: {news['description']} \n\
+                                Country: {news['country']}\n\n\n")
                             if len(news['content']) >= 1800:
                                 content = news['content'][0:1800]
                             else:
                                 content = news['content']
-                            
-                            creators = news['creator']
-                            if news['creator']: 
-                                News.objects.create(
-                                    title = news['title'], 
-                                    link = news['link'],
-                                    news_creator = creators,
-                                    image_url = news['image_url'],
-                                    pub_date = news['pubDate'],
-                                    update_date = news['pubDate'],
-                                    description = news['description'],
-                                    country = news['country'],
-                                    content = content
-                                )                     
+                            if news['creator']:
+                                creators = ""
+                            else:
+                                creators = news['creator']
+                            News.objects.create(
+                                title = news['title'], 
+                                link = news['link'],
+                                news_creator = creators,
+                                image_url = news['image_url'],
+                                pub_date = news['pubDate'],
+                                update_date = news['pubDate'],
+                                description = news['description'],
+                                country = news['country'],
+                                content = content
+                            )                     
                     except Exception as e:
                         print(e)
                         continue
