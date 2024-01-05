4e15d154acc4ae750f1511e4de29f25cce2aeff4
Nikolaev3Artem <artnik349@gmail.com>
2024-01-05 20:36:21 +0200

    Updated 0004 migration file

--- news/migrations/0004_remove_news_rating_news_rating.py
+++ news/migrations/0004_remove_news_rating_news_rating.py
@@ -10,10 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='news',
-            name='rating',
-        ),
         migrations.AddField(
             model_name='news',
             name='rating',
