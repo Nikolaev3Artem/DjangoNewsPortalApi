1094efc580ef7882e53a58356066e5cde4f48f05
ItzMaze <artnik349@gmail.com>
2024-01-26 15:43:02 +0200

    Fixing bugs with max quota on api key

--- news/management/commands/auto_translate.py
+++ news/management/commands/auto_translate.py
@@ -69,8 +69,10 @@ class Command(BaseCommand):
     help = 'Translating parsed news'
     
     def handle(self, *args, **options):
-        news = News.objects.all().filter(pub_date__gte = f'{str(datetime.datetime.now())[0:9]} 00:00:00', pub_date__lte = f'{str(datetime.datetime.now())[0:10]} 23:59:59', translated=False)
+        news = News.objects.all().filter(pub_date__gte = f'{str(datetime.datetime.now())[0:10]} 00:00:00', pub_date__lte = f'{str(datetime.datetime.now())[0:10]} 23:59:59', translated=False)
         print(news)
+        print(f'{str(datetime.datetime.now())[0:9]} 00:00:00')
+        print(f'{str(datetime.datetime.now())[0:9]} 23:59:59')
         for i in range(0,3):
             print(f'i: {i}')
             if len(news) != 0:
